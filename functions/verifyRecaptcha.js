16dd232c7c174932c80e6d6a93a08c5598c319d6
alaaeldin-92 <alaaeldin.92@outlook.com>
2024-08-21 16:58:20 +0400

    Update verifyRecaptcha.js

--- functions/verifyRecaptcha.js
+++ functions/verifyRecaptcha.js
@@ -3,14 +3,21 @@ const axios = require("axios");
 exports.handler = async function (event, context) {
   const { token } = JSON.parse(event.body);
 
-  const secretKey = "6LdokiQqAAAAAK6eGO9I6gWGGzUO5ilxi3UwR0AW";
+  const requestBody = {
+    event: {
+      token: token,
+      expectedAction: "USER_ACTION", // Optional
+      siteKey: "6LdokiQqAAAAAIvI20jxHA3mbRBAsVX_9M_yCQZu",
+    },
+  };
 
-  const verificationUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${token}`;
+  const apiKey = "AIzaSyB0Rf7vViIR06uTHlwSABIlZBtc1FDyM1Q"; // Replace with your API key
+  const verificationUrl = `https://recaptchaenterprise.googleapis.com/v1/projects/tripify-ai-planner/assessments?key=${apiKey}`;
 
   try {
-    const response = await axios.post(verificationUrl);
+    const response = await axios.post(verificationUrl, requestBody);
 
-    if (response.data.success) {
+    if (response.data.tokenProperties.valid) {
       return {
         statusCode: 200,
         body: JSON.stringify({ verified: true }),
